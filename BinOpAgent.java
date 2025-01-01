4aae6ed9045e30b5cf777aa39af61a1e3a0a56ae
asher <asherpeer@MacBook-Pro-sl-Asher.local>
2025-01-01 15:28:03 +0200

    Update

--- BinOpAgent.java
+++ BinOpAgent.java
@@ -1,6 +1,71 @@
 package test;
 
-public class BinOpAgent{
+import java.util.function.BinaryOperator;
 
-    
+public class BinOpAgent implements Agent {
+    private Node agent;
+    private Topic topicA;
+    private Topic topicB;
+    private Topic topicR;
+    private Double left;
+    private Double right;
+    private BinaryOperator<Double> operator;
+
+
+    public BinOpAgent(String agent,String topicA, String topicB, String topicR , BinaryOperator<Double> operator ){
+        this.agent = new Node(agent);
+        this.topicA = new Topic(topicA);
+        this.topicB = new Topic(topicB);
+        this.topicR = new Topic(topicR);
+        this.operator = operator;
+
+        Topic a = new Topic(topicA);
+        a.subscribe((Agent) this.agent);
+        Topic b = new Topic(topicB);
+        b.subscribe((Agent) this.agent);
+        Topic r = new Topic(topicR);
+        r.addPublisher((Agent) this.agent);
+
+    }
+
+
+    @Override
+    public String getName() {
+        return "BinOpAgent";
+    }
+
+    @Override
+    public void reset() {
+        this.left = 0.0;
+        this.right = 0.0;
+
+    }
+
+    @Override
+    public void callback(String topic, Message msg) {
+       if( Double.isNaN(msg.asDouble))
+           return;
+
+       if (topic.equals(this.topicA.name)){
+           left = msg.asDouble;
+       }
+       else if (topic.equals(this.topicB.name)){
+           right = msg.asDouble;
+       }
+       double test=operator.apply(left, right);
+       this.topicR.publish(new Message(Double.toString(test)));
+
+    }
+
+    @Override
+    public void close() {
+        left=null;
+        right=null;
+        topicA.unsubscribe((Agent) this.agent);
+        topicB.unsubscribe((Agent) this.agent);
+        topicR.unsubscribe((Agent) this.agent);
+        topicA = null;
+        topicB = null;
+        topicR = null;
+    }
 }
--- Node.java
+++ Node.java
@@ -1,12 +1,78 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 
 public class Node {
     private String name;
     private List<Node> edges;
     private Message msg;
+
+    public Node(String a) {
+        name = a;
+        edges = new ArrayList<>();
+        msg = null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEdges(List<Node> edges) {
+        this.edges = edges;
+    }
+
+    public List<Node> getEdges() {
+        return edges;
+    }
+
+    public void setMsg(Message msg) {
+        this.msg = msg;
+    }
+
+    public Message getMsg() {
+        return msg;
+    }
+
+    public void addEdge(Node b) {
+        edges.add(b);
+    }
+
+
+    public boolean hasCycles() {
+        Set<Node> visited = new HashSet<>();
+        Set<Node> recursionStack = new HashSet<>();
+        return hasCyclesHelper(this, visited, recursionStack);
+    }
+
+    private boolean hasCyclesHelper(Node current, Set<Node> visited, Set<Node> recursionStack) {
+        if (recursionStack.contains(current)) {
+            return true;
+        }
+
+
+        if (visited.contains(current)) {
+            return false;
+        }
+
+        visited.add(current);
+        recursionStack.add(current);
+
+
+        for (Node neighbor : current.getEdges()) {
+            if (hasCyclesHelper(neighbor, visited, recursionStack)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 
